14a4a2ffbd3f1d91e333b1811c8a24def9742e26
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2015-03-14 16:18:04 +0100

    4 leds

--- web/led.dart
+++ web/led.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2014, Nicolas François
+// Copyright (c) 2014-2015, Nicolas François
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,28 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Example from https://github.com/jgautier/firmata/blob/master/examples/blink.js
-
 import 'dart:html';
 import 'package:firmata/firmata_chrome.dart';
 
+final led1Pin = 4;
+final led2Pin = 5;
+final led3Pin = 6;
+final led4Pin = 7;
+
+final offCss = 'btn-default';
+
+final pinsId = {led1Pin : "led1" , led2Pin: "led2", led3Pin : "led3", led4Pin: "led4"};
+final pinStates = {led1Pin : false, led2Pin: false, led3Pin:false, led4Pin:false};
+final pinClasses = {led1Pin : 'btn-danger', led2Pin: 'btn-success', led3Pin:'btn-success', led4Pin:'btn-danger'};
+final Map<int, ButtonElement> ledsButtons = {};
 
 main(){
-  ButtonElement led1Button = querySelector("#led1");
-  final led1Pin = 4;
-
+ 
   detect().then((board){
-    print("Connected");
-    
-    bool led1On = false;
-    board.pinMode(led1Pin, PinModes.OUTPUT);
-    
-    led1Button.onClick.listen((_){
-      led1On = !led1On;
-      led1Button.text = led1On ? "ON" : "OFF";
-      board.digitalWrite(led1Pin, led1On ? PinValue.HIGH : PinValue.LOW);
-    }).onError(print);
-
+    // Init leds
+    pinsId.forEach((pin, id){
+      // Arduino init
+      board.pinMode(pin, PinModes.OUTPUT);
+      // UI init
+      var led = querySelector("#$id")..onClick.listen((_) => toggleButton(board, pin));
+      ledsButtons[pin] = led;
+    });
   });
+}
 
+toggleButton(Board b, pin){
+  // Change states
+  pinStates[pin] = !pinStates[pin];
+  b.digitalWrite(pin, pinStates[pin] ? PinValue.HIGH : PinValue.LOW); 
+  // UI changes
+  ledsButtons[pin].classes..toggle(offCss)..toggle(pinClasses[pin]); 
 }
