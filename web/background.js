87d935378c82c83287eec5aab831cde91c3e7693
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2015-03-18 01:15:49 +0100

    Rename

--- web/background.js
+++ web/background.js
@@ -1,6 +1,6 @@
 
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('led.html', {
+  chrome.app.window.create('ArduinoManager.html', {
     'id': '_mainWindow', 'bounds': {'width': 800, 'height': 600 }
   });
 });
