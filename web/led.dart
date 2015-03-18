34236c725315719ed48d8eabf043254f9d81b1b3
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2015-03-18 01:05:55 +0100

    read button states

--- web/led.dart
+++ web/led.dart
@@ -14,15 +14,25 @@
 
 import 'dart:html';
 import 'package:firmata/firmata_chrome.dart';
+import 'dart:async';
+
+
+final p1Pin = 2;
+final p2Pin = 3;
 
 final led1Pin = 4;
 final led2Pin = 5;
 final led3Pin = 6;
 final led4Pin = 7;
 
+// Buttons
+final buttonsIds = {p1Pin : "p1", p2Pin : "p2"};
+final Map<int, InputElement>  buttonsInputs = {};
+
+// Leds
 final offCss = 'btn-default';
 
-final pinsId = {led1Pin : "led1" , led2Pin: "led2", led3Pin : "led3", led4Pin: "led4"};
+final pinsIds = {led1Pin : "led1" , led2Pin: "led2", led3Pin : "led3", led4Pin: "led4"};
 final pinStates = {led1Pin : false, led2Pin: false, led3Pin:false, led4Pin:false};
 final pinClasses = {led1Pin : 'btn-danger', led2Pin: 'btn-success', led3Pin:'btn-success', led4Pin:'btn-danger'};
 final Map<int, ButtonElement> ledsButtons = {};
@@ -31,20 +41,34 @@ main(){
  
   detect().then((board){
     // Init leds
-    pinsId.forEach((pin, id){
+    pinsIds.forEach((pin, id){
       // Arduino init
       board.pinMode(pin, PinModes.OUTPUT);
       // UI init
-      var led = querySelector("#$id")..onClick.listen((_) => toggleButton(board, pin));
+      var led = querySelector("#$id")..onClick.listen((_) => toggleLedButton(board, pin));
       ledsButtons[pin] = led;
     });
+    // Init buttons
+    buttonsIds.forEach((pin, id){
+      // Arduino init
+      board.pinMode(pin, PinModes.INPUT);
+      // UI init
+      var button = querySelector("#$id");
+      buttonsInputs[pin] = button;
+    });
+    board.onDigitalRead.listen(toggleButtonInput);
   });
 }
 
-toggleButton(Board b, pin){
+toggleLedButton(Board b, pin){
   // Change states
   pinStates[pin] = !pinStates[pin];
   b.digitalWrite(pin, pinStates[pin] ? PinValue.HIGH : PinValue.LOW); 
   // UI changes
   ledsButtons[pin].classes..toggle(offCss)..toggle(pinClasses[pin]); 
+}
+
+toggleButtonInput(PinState pinState){
+  // UI changes
+  buttonsInputs[pinState.pin].value = pinState.value == 0 ? "ON" : "OFF";
 }
